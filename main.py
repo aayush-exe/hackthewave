9153bbffa7ba78121d686a081ffb575f93244799
savagesausage04 <kyleh1104@gmail.com>
2023-10-21 12:36:14 -0700

    generation + image

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+import openai
+openai.api_key = "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"
+
+user_input = input("Talk to the waifu!: ")
+completion_input = user_input + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
+                                "Seperate each section with two enter statements. "
+chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":
+    completion_input}])
+content = chat_completion['choices'][0]['message']['content']
+sections = content.split("\n\n")
+anime_response = sections[0]
+ingredients = sections[1]
+instructions = sections[2]
+
+anime_text = user_input + " Make it anime style, aesthetic, and with no text."
+response = openai.Image.create(prompt=anime_text, n=1, size="1024x1024")
+image_url = response['data'][0]['url']
+
+
+# Print the content
+print(anime_response)
+print(ingredients)
+print(instructions)
+print(image_url)
+
+
