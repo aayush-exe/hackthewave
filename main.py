e68b22e9c5fb77e43869c3766688e896cd94c23c
Aayush <66886829+aayush-exe@users.noreply.github.com>
2023-10-21 14:53:40 -0700

    Added anivl uplink

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import openai
-#import anvil.server
-#anvil.server.connect("server_G2CT33LWBAYSXBZEJY57QWW6-DK36JKLLPLGZLZCW")
+import anvil.server
+anvil.server.connect("server_JI76JEO7OH75THQBUAEVU44J-DK36JKLLPLGZLZCW")
 openai.api_key = "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"
 
 #completion_input = prompt + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
                                 #"Seperate each section with two enter statements. "
-#@anvil.server.callable
+@anvil.server.callable
 def get_text(prommpt):
     completion_input = prommpt + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
                                 "Seperate each section with two enter statements. "
@@ -14,18 +14,18 @@ def get_text(prommpt):
     content = chat_completion['choices'][0]['message']['content']
     return content
 
-get_text("Give me a steak recipe")
-'''anime_text = user_input + " and make the art style similar to My Hero Academia. Have no text in the photo. Make the photo aesthetically pleasing. Make the colors pastel."
-response = openai.Image.create(prompt=anime_text, n=1, size="1024x1024")
-image_url = response['data'][0]['url']
+# get_text("Give me a steak recipe")
+# '''anime_text = user_input + " and make the art style similar to My Hero Academia. Have no text in the photo. Make the photo aesthetically pleasing. Make the colors pastel."
+# response = openai.Image.create(prompt=anime_text, n=1, size="1024x1024")
+# image_url = response['data'][0]['url']
 
 
-# Print the content
-print(anime_response)
-print(ingredients)
-print(instructions)
-print(image_url)'''
+# # Print the content
+# print(anime_response)
+# print(ingredients)
+# print(instructions)
+# print(image_url)'''
 
-#anvil.server.wait_forever()
+anvil.server.wait_forever()
 
 
