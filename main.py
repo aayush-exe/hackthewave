c3af224b8067451de56b060658e4af78b87acbed
savagesausage04 <kyleh1104@gmail.com>
2023-10-21 14:49:15 -0700

    yes

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-import os
-
 import openai
-openai.api_key = "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"
+#import anvil.server
+#anvil.server.connect("server_G2CT33LWBAYSXBZEJY57QWW6-DK36JKLLPLGZLZCW")
+openai.api_key = "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"
 
-user_input = input("Talk to the waifu!: ")
-completion_input = user_input + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
+#completion_input = prompt + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
+                                #"Seperate each section with two enter statements. "
+#@anvil.server.callable
+def get_text(prommpt):
+    completion_input = prommpt + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
                                 "Seperate each section with two enter statements. "
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":
     completion_input}])
-content = chat_completion['choices'][0]['message']['content']
-sections = content.split("\n\n")
-anime_response = sections[0]
-ingredients = sections[1]
-instructions = sections[2]
+    content = chat_completion['choices'][0]['message']['content']
+    return content
 
-anime_text = user_input + " Make it anime style, aesthetic, and with no text."
+get_text("Give me a steak recipe")
+'''anime_text = user_input + " and make the art style similar to My Hero Academia. Have no text in the photo. Make the photo aesthetically pleasing. Make the colors pastel."
 response = openai.Image.create(prompt=anime_text, n=1, size="1024x1024")
 image_url = response['data'][0]['url']
 
@@ -23,6 +24,8 @@ image_url = response['data'][0]['url']
 print(anime_response)
 print(ingredients)
 print(instructions)
-print(image_url)
+print(image_url)'''
+
+#anvil.server.wait_forever()
 
 
