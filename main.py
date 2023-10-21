be39cba8c8f3391a9f6130dc89b02f179354544f
Aayush <66886829+aayush-exe@users.noreply.github.com>
2023-10-21 16:11:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import openai
-#import anvil.server
-#anvil.server.connect("server_JI76JEO7OH75THQBUAEVU44J-DK36JKLLPLGZLZCW")
-openai.api_key = "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"
+import anvil.server
+anvil.server.connect("server_JI76JEO7OH75THQBUAEVU44J-DK36JKLLPLGZLZCW")
+openai.api_key = "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"
 buttonShow = False
 #completion_input = prompt + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
-                                #"Seperate each section with two enter statements. "
-#@anvil.server.callable
+                
+UsersName = ""
+
+@anvil.server.callable
+
 def get_text(prommpt):
     if "recipe" in prommpt:
-        completion_input = prommpt + " There should be 3 sections: 'Here is your recipe! :)',  an 'Ingredients:' section, and a short 'Instuctions:' section. " \
+        completion_input = prommpt + " There should be 2 sections: an 'Ingredients:' section, and a short 'Instuctions:' section. " \
                                        "Seperate each section with two enter statements. Don't put spaces for the list in 'Instructions'"
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":
     completion_input}])
         content = chat_completion['choices'][0]['message']['content']
         sections = content.split("\n\n")
-        intro = sections[0]
-        ingredient = sections[1]
+        ingredients = sections[1]
         instruction = sections[2] + sections[3]
-        return intro, ingredient, instruction
+        return "Here is your recipe"
     else:
         completion_input = prommpt + " Speak like a cute chef. Limit to one sentence"
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":
@@ -25,9 +27,10 @@ def get_text(prommpt):
         content = chat_completion['choices'][0]['message']['content']
         return content
 
+@anvil.server.callable
+def send_name(name):
+    UsersName = name
 
-
-print(get_text("Hello! How are you"))
 #def greetings():
 
 # get_text("Give me a steak recipe")
@@ -42,6 +45,6 @@ print(get_text("Hello! How are you"))
 # print(instructions)
 # print(image_url)'''
 
-#anvil.server.wait_forever()
+anvil.server.wait_forever()
 
 
