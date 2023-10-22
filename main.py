9b3baf51b8062c6ef1441d6ab7bd6b846b20741c
Aayush <66886829+aayush-exe@users.noreply.github.com>
2023-10-21 17:31:43 -0700

    images added + prompt_chage
    ehhe

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 import openai
 from random import randint
 import anvil.server
-#anvil.server.connect("server_JI76JEO7OH75THQBUAEVU44J-DK36JKLLPLGZLZCW")
-openai.api_key = "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"
+
 buttonShow = False
 #completion_input = prompt + " There should be 3 sections: a response,  an 'Ingredients:' section, and a short 'Instuctions:' section." \
                 
 UsersName = ""
+
+@anvil.server.callable
+def send_name(name):
+    global UsersName
+    UsersName = name
+
 recipe_greetings = [
-    f"Hello {UsersName}, it's time to cook up something delicious!",
-    "Good day, let's whip up a scrumptious dish!",
-    f"Hey {UsersName}, ready to get your cook on?",
-    "Greetings, it's cooking time, my friend!",
-    "Hi, let's dive into the world of culinary creations!",
+    f"Let's get started, {UsersName}, it's time to cook up something delicious!",
+    "Okay, let's whip up a scrumptious dish!",
+    f"Alright {UsersName}, ready to get your cook on?",
+    "Okay then, it's cooking time!",
+    "So, let's dive into the world of culinary creations!",
     f"Hello {UsersName}, let's work our kitchen magic together!",
-    f"Hey {UsersName}, let's embark on a culinary adventure!",
+    f"Okay {UsersName}, let's embark on a culinary adventure!",
     "Ohayo, let's make something tasty and memorable!",
-    "Hello, are you ready to craft a delectable recipe?",
-    f"Hey {UsersName}, let's create a mouthwatering masterpiece today!"
+    "Alright, are you ready to craft a delectable recipe?",
+    f"Okay {UsersName}, let's create a mouthwatering masterpiece today!"
 ]
 
-#@anvil.server.callable
-
+@anvil.server.callable
 def get_text(prommpt):
     if "recipe" in prommpt:
-        return recipe_greetings[randint(0,9)]
+        return True, recipe_greetings[randint(0,9)]
     else:
-        completion_input = prommpt + " Speak like a cute chef. Limit to one sentence"
+        completion_input = prommpt + "Your name is Misaki, and you are a cute and friendly chef. You're happy to make conversation and help your friend, "+UsersName+" with whatever they might need. Limit to one sentence if you can, but retain a friendly, down-to-earth tone. Seem personable and friendly, and make some conversation if they are too. Name the user by name if possible, as a reminder the name is "+UsersName+"."
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":
     completion_input}])
         content = chat_completion['choices'][0]['message']['content']
         return False, content
 
+@anvil.server.callable
 def get_recipe(prommpt):
     completion_input = prommpt + " There should be 2 sections: an 'Ingredients:' section, and a short 'Instuctions:' section with no enters between each step" \
                                    "Seperate each section with two enter statements. Don't put a space between the header."
@@ -46,12 +51,8 @@ def get_recipe(prommpt):
 
 
 
-print(get_text("Give me burger recipe"))
-print(get_recipe("Give me burger recipe"))
 
-#@anvil.server.callable
-def send_name(name):
-    UsersName = name
+
 
 #def greetings():
 
@@ -67,6 +68,6 @@ def send_name(name):
 # print(instructions)
 # print(image_url)'''
 
-#anvil.server.wait_forever()
+anvil.server.wait_forever()
 
 
